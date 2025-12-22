18b85c17ec8bd5e52c0e0c65340d92e94ca68840
zieeco <zieecosam@gmail.com>
2025-12-22 17:52:29 +0100

    feat: update to add core files

--- backend/apps/payments/apps.py
+++ backend/apps/payments/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
-
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.payments'
-    verbose_name = 'Payments'
+
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     
-    # Local apps
+    # Custom apps
     'apps.core',
     'apps.products',
     'apps.cart',
