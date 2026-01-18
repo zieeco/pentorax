0e52b6c7945284ec63063a934542cf74ae0fb641
zieeco <zieecosam@gmail.com>
2026-01-18 18:33:48 +0100

    feat: remove extra prefix from route

--- backend/apps/payments/urls.py
+++ backend/apps/payments/urls.py
@@ -8,7 +8,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PaymentViewSet
 
 router = DefaultRouter()
-router.register(r"payments", PaymentViewSet, basename="payment")
+router.register(r"", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("", include(router.urls)),
