bb81406ce3a95affc9e51f7f89d8768e2b34731f
zieeco <zieecosam@gmail.com>
2026-03-29 00:05:01 +0100

    refactor: format and lint frontend_next/src/providers/ThemeProvider.tsx

--- frontend_next/src/providers/ThemeProvider.tsx
+++ frontend_next/src/providers/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import * as React from 'react';
 
 /**
  * ThemeProvider component using next-themes
