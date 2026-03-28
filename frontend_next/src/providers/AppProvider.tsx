a8b97bec2c694489c0a30e6d98d95a9ecf26f731
zieeco <zieecosam@gmail.com>
2026-03-29 00:04:47 +0100

    refactor: format and lint frontend_next/src/providers/AppProvider.tsx

--- frontend_next/src/providers/AppProvider.tsx
+++ frontend_next/src/providers/AppProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { Toaster } from '@/components/ui/sonner';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/react-query';
 import { ThemeProvider } from './ThemeProvider';
 
