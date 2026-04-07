b508062c201f60ce92141c5591bdc06c5ed543a7
zieeco <zieecosam@gmail.com>
2026-04-07 16:38:36 +0100

    refactor: update core serializers

--- backend/apps/core/serializers.py
+++ backend/apps/core/serializers.py
@@ -16,6 +16,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ["email", "password", "name", "first_name", "last_name"]
 
+    def validate_email(self, value):
+        if User.objects.filter(username=value).exists() or User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
     def create(self, validated_data):
         name = validated_data.pop("name", "")
         email = validated_data["email"]
